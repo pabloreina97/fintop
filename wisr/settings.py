821b13f1aa47ec645336434c9f987dab22a856c3
PabloReina <pabloreinagalvez@gmail.com>
2024-01-16 14:56:56 +0100

    Sustituir por variables de entorno

--- wisr/settings.py
+++ wisr/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t2(@%+(v!u#@j%-d^28#3ak=9gf%xm5!c6u4i!u=14h!$2^vt-'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'wisr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fintop',
-        'USER': 'fintop_admin',
-        'PASSWORD': 'Budgetum$',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
