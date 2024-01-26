f8523182cd71c16e2692286322d5d2d1eda0658d
PabloReina <pabloreinagalvez@gmail.com>
2024-01-26 11:20:39 +0100

    Modificar settings Celery

--- wisrapp/tasks.py
+++ wisrapp/tasks.py
@@ -8,3 +8,8 @@ def sync_transactions():
     vista = SincronizarTransaccionesView()
     vista.sincronizar_transacciones()
     print("Transacciones sincronizadas")
+
+
+@shared_task
+def sumar(x, y):
+    return x + y
