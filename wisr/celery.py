a87bafaebd3875e0f21a1969df5cc042a2a2324e
PabloReina <pabloreinagalvez@gmail.com>
2024-02-05 13:57:08 +0100

    Modificar configuración celery

--- wisr/celery.py
+++ wisr/celery.py
@@ -4,16 +4,25 @@ from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
 
-# establecer el valor predeterminado de Django para la aplicación de Celery
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wisr.settings')
 
 app = Celery('wisr')
 
-# Usar la configuración de Django para configurar Celery
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Cargar tareas definidas en cada aplicación
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
 
 # Programar tareas periódicas
 app.conf.beat_schedule = {
--- wisr/settings.py
+++ wisr/settings.py
@@ -148,4 +148,3 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Europe/Berlin'
