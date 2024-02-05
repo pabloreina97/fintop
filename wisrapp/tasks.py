161f1a1dd8f3a469388073ae17e7303e32f039d0
PabloReina <pabloreinagalvez@gmail.com>
2024-02-05 14:36:07 +0100

    Modificar invocacion funciones tasks

--- wisrapp/tasks.py
+++ wisrapp/tasks.py
@@ -1,12 +1,13 @@
 from celery import shared_task
+from wisrapp.services import SyncManager, TokenManager
 
 from wisrapp.views import AuthRefresh, SyncTransactionsView
 
 
 @shared_task
 def sync_transactions():
-    AuthRefresh.get()
-    SyncTransactionsView().get()
+    TokenManager.refresh_token()
+    SyncManager.sincronizar_transacciones()
     print("Transacciones sincronizadas")
 
 
